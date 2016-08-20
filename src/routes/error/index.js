cee6074b1f4849ea9f53c1cb80013ca46296ae68
Saptarshi Chatterjee <saptarshi.chatterjee@paytm.com>
2016-08-20 18:05:17 +0530

    local storage working

--- src/routes/error/index.js
+++ src/routes/error/index.js
@@ -1,5 +1,3 @@
-import { getParameterByName, addParamToUrl } from '../../core/utils';
-
 import React from 'react';
 import App from '../../components/App';
 import ErrorPage from './ErrorPage';
@@ -8,23 +6,11 @@ export default {
 
   path: '/error',
 
-  action({ render, context, error, path }) {
-    if (process.env.BROWSER) {
-      path = window.location.href; //eslint-disable-line
-      const refreshedOnce = getParameterByName('refreshed', path);
-      // Hard refresh the page only once if on error.
-      // Stop from refreshing again and again on error.
-      if (!refreshedOnce) {
-        const refreshpath = addParamToUrl(path, { refreshed: 1 });
-        window.location.href = refreshpath;
-        return;
-      }
-    }
+  action({ render, context, error }) {
     render(
       <App context={context} error={error}>
         <ErrorPage error={error} />
-      </App>,
-      error.status || 500
+      </App>
     );
   },
 
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -6,7 +6,6 @@ import { getParameterByName } from '../../core/utils';
 import { QUESTION_URL_PARAM } from '../../constants';
 import fetch from '../../core/fetch';
 import { questionPath } from '../../core/urls';
-// import { updateGaKey, updateFooter } from '../../actions';
 
 // import _ from 'lodash';
 
@@ -22,11 +21,19 @@ class Home extends Component { // eslint-disable-line react/prefer-stateless-fun
     if (question) {
       const response = await fetch(`${questionPath}${question}`);
       const data = await response.json();
-      alert(JSON.stringify(data));
+      this.initLocalStore(data.id);
     }
   }
 
   componentWillReceiveProps() {
+  }
+
+  initLocalStore(qId) {
+    const initState = {
+      id: qId,
+      started: false
+    };
+    localStorage.setItem(`${QUESTION_URL_PARAM}-${qId}`, JSON.stringify(initState));
   }
 
   render() {
@@ -54,3 +61,45 @@ export default withStyles(s)(connect((state) => {
     }
   };
 })(Home));
+
+/*
+
+Concepts:
+
+- Questionnaire
+
+- A Questionnaire has a title and a description
+
+- A questionnaire has a defined sequence of question that need to be answered
+
+- Question
+
+- 3 different types of questions are supported: Freetext, Multiple choice (single answer), Multiple
+
+choice (multi answer)
+
+- Questions can be flagged to have a mandatory answer
+
+- Question has a description
+
+- For Multiple Choice, each choice has a descriptive label that is shown to the user
+
+Requirements:
+
+- As a user when I start the questionnaire I see a view with title, description and a CTA to start
+
+- As a user I can answers questions very swiftly so that I don’t feel like I am wasting my time
+
+- As a user I want the questionnaire to work dependable when I have bad network connectivity
+
+- The questionnaire definition is based on a JSON structure that the frontend uses to drive the questions,
+
+a good reference for a JSON might be here: http://docs.typeform.io/docs/introduction
+
+- As a user I can go back to a previous question without losing the answers I have given in a current
+
+question
+
+- Mobile First, one of the pages contains an SVG, uses JS ES 6-7, naming convention for CSS
+
+*/
