7cac1345645df0718da08919f0701abf5eff9d1b
Saptarshi Chatterjee <saptarshi.chatterjee@paytm.com>
2016-08-23 19:24:23 +0530

    more tests

--- src/components/SurveyView/SurveyView.test.js
+++ src/components/SurveyView/SurveyView.test.js
@@ -16,14 +16,34 @@ describe('SurveyView', () => {
     expect(wrapper.children().length).to.equal(1); // Shallow wrapper returns only one child
   });
 
-  it('View Should have total 5 divs outside nested JSX', () => {
+  it('View Should have atlest 5 divs', () => {
     const wrapper = mount(<SurveyView />);
-    expect(wrapper.find('div').length).to.equal(5);
+    expect(wrapper.find('div').length).to.be.at.least(5);
   });
 
   it('Initial state Should have blank userProgress Array', () => {
     const wrapper = shallow(<SurveyView />);
     expect(wrapper.state()).to.eql({ userProgress: [] }); // deep equal check with eql
+  });
+
+  it('On servey start question,totalQstn,userProgress,qIndex Should be available in state', () => {
+    const wrapper = mount(<SurveyView />);
+    const questionMock = require('../../questinaire/question-1.json'); //eslint-disable-line
+    // window.localStorage no defined . Mock it
+    const localStorage = {};
+    localStorage.setItem = () => true;
+    localStorage.getItem = () => null;
+    GLOBAL.localStorage = localStorage;
+    // mock ends
+    wrapper.setState({ qid: 1, entireQstn: questionMock, qstnHash: 1 });
+    wrapper.find('button').simulate('click');
+    
+    expect(wrapper.state()).to.include.keys(['question', 'totalQstn', 'userProgress', 'qIndex']);
+    expect(wrapper.state()).to.include({
+      entireQstn: questionMock,
+      totalQstn: 4,
+      qIndex: 0
+    });
   });
 
 });
